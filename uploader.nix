c2d220da06014b4fa4a80ae5f74589f9ddd56c35
Tikunov Rodion <rodion@itrm.ru>
2016-09-21 02:11:44 +0300

    Removed unnecessary directives

--- uploader.nix
+++ uploader.nix
@@ -31,39 +31,24 @@ services.nginx.httpConfig = ''
 '';
 
 environment.systemPackages = [
-  ( with import <nixpkgs> {};
-    let dependencies = rec {
-        _djangorestframework = pkgs.python35Packages.djangorestframework.override rec {
-            name = "djangorestframework-3.3.2";
-            src = fetchurl {
-                url = "https://pypi.python.org/packages/source/d/djangorestframework/${name}.tar.gz";
-                md5 = "f45b14a65e95b85216018bd009341683";
-            };
-
-            propagatedBuildInputs = [
-                pkgs.python35
-                pkgs.python35Packages.django_1_8
-                pkgs.python35Packages.gunicorn
-            ];
-            doCheck = false;
+  ( let _djangorestframework = pkgs.python35Packages.buildPythonPackage rec {
+        name = "djangorestframework-${version}";
+        version = "3.3.2";
+        src = pkgs.fetchurl {
+            url = "https://pypi.python.org/packages/source/d/djangorestframework/${name}.tar.gz";
+            md5 = "f45b14a65e95b85216018bd009341683";
         };
+
+        propagatedBuildInputs = [
+            pkgs.python35
+            pkgs.python35Packages.django_1_8
+            pkgs.python35Packages.gunicorn
+        ];
+        doCheck = false;
     };
 
-    in with dependencies;
-    stdenv.mkDerivation rec {
-        name = "env";
-
-        env = buildEnv { name = name; paths = buildInputs; };
-        builder = builtins.toFile "builder.sh" ''
-            source $stdenv/setup; ln -s $env $out
-        '';
-
-        buildInputs = [
-            (python35.buildEnv.override {
-                ignoreCollisions = true;
-                extraLibs = with python35Packages; [ _djangorestframework ];
-            })
-        ];
+    in pkgs.python35.buildEnv.override rec {
+        extraLibs = [ _djangorestframework ];
     }
   )
 ];
@@ -74,15 +59,14 @@ systemd.services.uploader = {
        WorkingDirectory = "/app/uploader";
        PIDFile = "/tmp/uploader.pid";
        Type = "forking";
-       User = "uploader";
        KillMode = "process";
        ExecStartPre = [
-           '' ${config.system.path}/bin/chown -R uploader /app/uploader ''
            '' ${config.system.path}/bin/python3 /app/uploader/manage.py migrate --noinput ''
            '' ${config.system.path}/bin/python3 /app/uploader/manage.py collectstatic --noinput ''
            '' ${config.system.path}/bin/python3 /app/uploader/manage.py test --noinput ''
+           '' ${config.system.path}/bin/chown -R uploader /app/uploader ''
        ];
-       ExecStart = '' ${config.system.path}/bin/gunicorn  --workers 1 --bind=127.0.0.1:8000 --pid=/tmp/uploader.pid --pythonpath=/app/uploader --error-logfile=/home/uploader/uploader.error.log --daemon uploader.wsgi:application '';
+       ExecStart = '' ${config.system.path}/bin/gunicorn --user uploader --workers 1 --bind=127.0.0.1:8000 --pid=/tmp/uploader.pid --pythonpath=/app/uploader --error-logfile=/home/uploader/uploader.error.log --daemon uploader.wsgi:application '';
    };
 };
 systemd.services.uploader.enable = true;
